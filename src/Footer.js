1b7f0230d3a8bb68f27fe91d28f926c6660bd49c
Roberto Santacroce <112706229+rsantacroce@users.noreply.github.com>
2024-05-03 12:09:15 +0200

    added github link and adjust title

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      github.com/...
+      <a href="https://github.com/santacroce-tech/stk-deposit-ui" target="_blank" rel="noopener noreferrer">
+        View on GitHub
+      </a>
     </footer>
   );
 };
